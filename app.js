971f0d3f296826354ab145bc3b1220e5223703cf
kmh339 <kmh339@gmail.com>
2019-12-15 23:24:12 +0900

    .

---
+++ app.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event) {
+  //console.log(event);
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+}
